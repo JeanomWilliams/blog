57c6f7f73e5434b7a311136eb7c4fbc77e4b28b2
JeanomWilliams <jeanomwilliams@gmail.com>
2025-10-16 10:25:20 +0100

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "body",
+    )
+#    search_fields = ("title", "body")
+
+admin.site.register(Post, PostAdmin)
+
+# Register your models here.
